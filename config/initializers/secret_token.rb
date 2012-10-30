74d80b2c3bf568530baca11c4ef63fd8d4d2629f
Cameron Webb <cwebb02@gmail.com>
2012-10-30 18:07:58 -0400

    whooooooo hooo.... slew of updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RidePeachy::Application.config.secret_token = 'a8fbd2785f33bc777a869db76f38083d6d1dd73cb1374a5f40bfc5369a7d52ddc05c1e220a74abf1864f675eed7c4ccc02d3063b9d39ce827fa9f576da1da1b9'
